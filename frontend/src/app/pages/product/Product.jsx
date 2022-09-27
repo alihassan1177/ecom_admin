dc6b1da5a2c96b92f691f74824b1454c13d2b428
alihassan1177 <ah927481@gmail.com>
2022-09-27 17:36:25 +0500

    "Added Create and Delete Row Functionality"

--- frontend/src/app/pages/product/Product.jsx
+++ frontend/src/app/pages/product/Product.jsx
@@ -3,7 +3,7 @@ import MainLayout from "../MainLayout";
 import { PageTitle, Loader } from "../../components/Page";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
-import { getDataFromApi, BASE_APP_URL } from "../../utils/Api";
+import { getDataFromApi, BASE_APP_URL, deleteData } from "../../utils/Api";
 import { HiPlus } from "react-icons/hi";
 
 export default function ProductPage() {
@@ -18,6 +18,11 @@ export default function ProductPage() {
     };
   }, []);
 
+  async function deleteRow(id) {
+    await deleteData(`product/delete/${id}`, user.token);
+    setProducts(await getDataFromApi("/products", user.token));
+  }
+
   return (
     <MainLayout>
       <PageTitle>Products</PageTitle>
@@ -31,7 +36,11 @@ export default function ProductPage() {
             <span>Create New Product</span> <HiPlus className="text-lg" />
           </Link>
         </header>
-        <table className="bg-white min-h-[150px] rounded border overflow-hidden relative p-2 w-full">
+        <table
+          className={`bg-white ${
+            products.length > 1 || products.length < 1 ? "min-h-[150px]" : ""
+          }  rounded border overflow-hidden relative p-2 w-full`}
+        >
           <thead className="bg-blue-800 text-white h-10">
             <tr>
               <th className="font-semibold">#</th>
@@ -50,39 +59,42 @@ export default function ProductPage() {
                 </td>
               </tr>
             ) : !(products.length > 0) ? (
-              "No Products Found"
+              <tr className="absolute left-1/2 top-16 -translate-x-1/2">
+                <td>No Products Found</td>
+              </tr>
             ) : (
               products.map((product, index) => {
                 return (
-                  <>
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{product.product_name}</td>
-                      <td className="hide">{product.product_code}</td>
-                      <td className="hide">{product.product_cat}</td>
-                      <td>
-                        <img
-                          className="w-32 py-2 object-cover mx-auto"
-                          src={`${BASE_APP_URL}storage/${product.product_img}`}
-                          alt={product.product_name}
-                        />
-                      </td>
-                      <td className="flex items-center gap-2 flex-wrap justify-center h-20 hide">
-                        <Link
-                          className="py-3 px-6 transition-colors rounded font-semibold bg-yellow-300 hover:bg-yellow-500"
-                          to="products/edit"
-                        >
-                          Edit
-                        </Link>
-                        <Link
-                          className="py-3 px-6 transition-colors rounded font-semibold text-white bg-red-500 hover:bg-red-700"
-                          to="products/delete"
-                        >
-                          Delete
-                        </Link>
-                      </td>
-                    </tr>
-                  </>
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{product.product_name}</td>
+                    <td className="hide">{product.product_code}</td>
+                    <td className="hide">{product.product_cat}</td>
+                    <td>
+                      <img
+                        className="w-32 py-2 object-cover mx-auto"
+                        src={`${BASE_APP_URL}storage/${product.product_img}`}
+                        alt={product.product_name}
+                      />
+                    </td>
+                    <td className="flex items-center gap-2 flex-wrap justify-center h-20 hide">
+                      <Link
+                        className="py-3 px-6 transition-colors rounded font-semibold bg-yellow-300 hover:bg-yellow-500"
+                        to={`products/edit/${product.id}`}
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => {
+                          deleteRow(product.id);
+                        }}
+                        className="py-3 px-6 transition-colors rounded font-semibold text-white bg-red-500 hover:bg-red-700"
+                        to="products/delete"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
                 );
               })
             )}
--- frontend/src/app/utils/Api.js
+++ frontend/src/app/utils/Api.js
@@ -25,13 +25,29 @@ export const getDataFromApi = async (URL, userToken) => {
 
 export const sendDataToApi = async (URL, userToken, data) => {
   try {
-    const response = await api.post(URL, {
+    const response = await api.post(URL, data, {
       method: "POST",
       headers: {
         Accept: "application/json",
         Authorization: `Bearer ${userToken}`,
       },
-      data: data,
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data.error);
+    }
+  }
+};
+
+export const deleteData = async (URL, userToken) => {
+  try {
+    const response = await api.post(URL, "", {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
     });
     console.log(response.data);
     return response.data;
